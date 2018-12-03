77d70bee53157ff12d7d85cffbd5730968472ed3
florsepulvedadv <florencia.sepulveda@davinci.edu.ar>
2018-12-03 12:17:29 -0300

    Borré el iframe del mapa estatico en mobile,

--- resources/views/huertas/show.blade.php
+++ resources/views/huertas/show.blade.php
@@ -263,9 +263,6 @@
 								<div id="map"></div>
 							</div>
 						</div>
-
-						<!-- Mapa mobile -->
-						<iframe class="d-block d-md-none" src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d26303.959583819764!2d-59.081505781772584!3d-34.50301235307769!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses!2sar!4v1542803566910" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
 					</div>
 				</div>
 
