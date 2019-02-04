0d5868d0eb1f21acafa20b67886b1db89f38d976
mrjouts <emiliano.hotes@davinci.edu.ar>
2019-02-04 20:43:35 -0300

    +

--- resources/views/cpanel/index.blade.php
+++ resources/views/cpanel/index.blade.php
@@ -4,37 +4,29 @@
 
 @section ('content')
 
+<div class="main-wrapper relative small-height">
+	<section class="container profile">
+
+		<h2 class="mb-4">Perfil del usuario tipo huerta</h2>
+
+		<div class="row">
 
 
-<p>huerta c panel</p>
-<p>huerta c panel</p>
-<p>huerta c panel</p>
-<p>huerta c panel</p>
-<p>huerta c panel</p>
-<p>huerta c panel</p>
-<p>huerta c panel</p>
-<p>huerta c panel</p>
-<p>huerta c panel</p>
-<p>huerta c panel</p>
-<p>huerta c panel</p>
-<p>huerta c panel</p>
-<p>huerta c panel</p>
-<p>huerta c panel</p>
-<p>huerta c panel</p>
-<p>huerta c panel</p>
-<p>huerta c panel</p>
-<p>huerta c panel</p>
-<p>huerta c panel</p>
-<p>huerta c panel</p>
-<p>huerta c panel</p>
-<p>huerta c panel</p>
-<p>huerta c panel</p>
-<p>huerta c panel</p>
-<p>huerta c panel</p>
-<p>huerta c panel</p>
-<p>huerta c panel</p>
-<p>huerta c panel</p>
-<p>huerta c panel</p>
-<p>huerta c panel</p>
+			<div class="col-3">
+				<ul class="list-group">
+					<li class="list-group-item">Perfil perfil</li>
+					<li class="list-group-item">Agregar un producto</li>
+					<li class="list-group-item">Ver pedidos</li>
+					<li class="list-group-item">Editar Huerta</li>
+					<li class="list-group-item">Zonas de envios</li>
+				</ul>
+			</div>
+
+			<div class="col-9">	
+				<h3>Detalles de la huerta</h3>
+			</div>
+		</div>
+	</section>
+</div>
 
 @endsection
