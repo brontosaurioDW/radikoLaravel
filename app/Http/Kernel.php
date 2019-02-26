f2a2d0f5018a9e7b602d080570f6a4bd21530917
Emiliano <emiliano@agenciacantalupe.com>
2019-02-26 09:47:42 -0300

    huertaq middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -60,6 +60,7 @@ class Kernel extends HttpKernel
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-		'admin' => \App\Http\Middleware\Admin::class,
+        'admin' => \App\Http\Middleware\Admin::class,
+		'huerta' => \App\Http\Middleware\Huerta::class
     ];
 }
