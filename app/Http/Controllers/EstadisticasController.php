cdef961531f9f0821d6707574045d421d8b5a172
lauramitono <mlauramitono@gmail.com>
2019-02-27 13:15:50 -0300

    Estadisticas

--- app/Http/Controllers/EstadisticasController.php
+++ app/Http/Controllers/EstadisticasController.php
@@ -10,22 +10,41 @@ class EstadisticasController extends Controller
     public function showEstadisticas(){
 
 		// Pedidos totales
-		$totalpedidos = DB::table('pedidos')->count();
+		$totalPedidos = DB::table('pedidos')->count();
 		
 		// Usuarios totales
-		$totalusuarios = DB::table('users')->count();
+		$totalUsuarios = DB::table('users')->count();
 		
 		// Huertas totales
-		$totalhuertas = DB::table('huertas')->count();
+		$totalHuertas = DB::table('huertas')->count();
 		
 		// Clientes totales
-		$totalclientes = DB::table('users')->where('tipo', 'cliente')->count();
+		$totalClientes = DB::table('users')->where('tipo', 'cliente')->count();
+		
+		// Estados usuarios CORREGIR
+		$estadosHuertas = DB::select("SELECT COUNT(id) as total, estado FROM users GROUP BY estado");
+		/*$estadosHuertas = DB::select("SELECT count(id) as total, users.estado FROM huertas LEFT JOIN users ON huertas.usuario_id = users.id GROUP BY users.estado");*/
+		
+		// Tipos de huertas
+		$totalBasicas = DB::table('huertas')->where('tipo_huerta_id', '1')->count();
+		$totalPremium = DB::table('huertas')->where('tipo_huerta_id', '2')->count();
+		
+		// Últimas huertas registradas
+		$ultimasHuertas = DB::select("SELECT * FROM huertas ORDER BY created_at DESC LIMIT 4");
+		
+		// Estados de los pedidos
+		$estadosPedidos = DB::select("SELECT count(id) as total, estados_pedidos.estado FROM pedidos LEFT JOIN estados_pedidos ON pedidos.id_estado_pedido = estados_pedidos.id_estado_pedido GROUP BY estados_pedidos.estado");
 
 		return view('admin.estadisticas', compact(
-			'totalpedidos', 
-			'totalusuarios', 
-			'totalhuertas', 
-			'totalclientes'
+			'totalPedidos', 
+			'totalUsuarios', 
+			'totalHuertas', 
+			'totalClientes',
+			'estadosHuertas',
+			'totalBasicas',
+			'totalPremium',
+			'ultimasHuertas',
+			'estadosPedidos'
 			));
 	}
 }
